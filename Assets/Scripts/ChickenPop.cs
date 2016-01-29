7476c7069388674d1ab4233f3407e71d4ad700cf
Cricroo <crizz13@hotmail.fr>
2016-01-29 23:11:34 +0100

    chickenPop ok

--- Assets/Scripts/ChickenPop.cs
+++ Assets/Scripts/ChickenPop.cs
@@ -4,13 +4,20 @@ using System.Collections.Generic;
 
 public class ChickenPop : MonoBehaviour {
     [SerializeField]
-    List<GameObject>  chickenGo;
+    int maxChickenNb;
+    GameObject chickenGo;
+    [SerializeField]
+    List<GameObject>  chickenGoList;
     [SerializeField]
     private float popTime;
     private int nbChickenTypes;
     private int chickenPickedRandomly;
+    private bool isAlreadyCancelled;
+    private int nbChickenInstanciated;
     // Use this for initialization
     void Start () {
+        nbChickenInstanciated = 0;
+        isAlreadyCancelled = false;
         nbChickenTypes = 3;//Placeholder
         //ChickenList: (0:Red, 1:Teal,2:Purple...)
         chickenPickedRandomly = Random.Range(0, nbChickenTypes);
@@ -19,15 +26,21 @@ public class ChickenPop : MonoBehaviour {
         {
             InvokeRepeating("SpawnChicken", popTime, popTime);
         }
+        
     }
 
     void SpawnChicken()
     {
-        chickenGo = Instantiate(Resources.Load("Prefabs/Chicken"+chickenPickedRandomly)) as GameObject;
-
+        chickenGo = Instantiate(chickenGoList[chickenPickedRandomly]);//as GameObject;
+        nbChickenInstanciated++;
+        chickenPickedRandomly = Random.Range(0, nbChickenTypes);
     }
     // Update is called once per frame
     void Update () {
-	
+	if(chickenGo != null && isAlreadyCancelled == false && nbChickenInstanciated == maxChickenNb)
+        {
+            isAlreadyCancelled = true;
+            CancelInvoke("SpawnChicken");
+        }
 	}
 }
