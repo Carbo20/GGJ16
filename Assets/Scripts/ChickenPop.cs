20cb9852363358b8ac1cd5219a54c2e9dcda3f82
Cricroo <crizz13@hotmail.fr>
2016-01-29 22:02:28 +0100

    popChicken

---
+++ Assets/Scripts/ChickenPop.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ChickenPop : MonoBehaviour {
+    [SerializeField]
+    List<GameObject>  chickenGo;
+    [SerializeField]
+    private float popTime;
+    private int nbChickenTypes;
+    private int chickenPickedRandomly;
+    // Use this for initialization
+    void Start () {
+        nbChickenTypes = 3;//Placeholder
+        //ChickenList: (0:Red, 1:Teal,2:Purple...)
+        chickenPickedRandomly = Random.Range(0, nbChickenTypes);
+        Debug.Log("chickenPickedRandomly: "+ chickenPickedRandomly);
+        if (chickenGo == null)
+        {
+            InvokeRepeating("SpawnChicken", popTime, popTime);
+        }
+    }
+
+    void SpawnChicken()
+    {
+        chickenGo = Instantiate(Resources.Load("Prefabs/Chicken"+chickenPickedRandomly)) as GameObject;
+
+    }
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
