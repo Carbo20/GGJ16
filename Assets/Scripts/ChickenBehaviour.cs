1760b8f8f9f80ec26197fc2bb60edaa6446cec18
U-otto-pc\otto <bastie_g@epitech.eu>
2016-01-30 03:36:10 +0100

    smarter chickens

--- Assets/Scripts/ChickenBehaviour.cs
+++ Assets/Scripts/ChickenBehaviour.cs
@@ -6,9 +6,16 @@ public class ChickenBehaviour : MonoBehaviour
     public float speed = 5f;
     public float countdown = 0.7f;
     public Vector2 direction;
+    public Vector2 henhouse;
+    public chickenState state;
+    public bool facingleft = true;
+    public enum chickenState { inHenhouse, Captured, Returning };
     // Use this for initialization
     void Start()
     {
+        //state = chickenState.inHenhouse;
+        //henhouse.Set(transform.position.x, transform.position.y);
+        henhouse.Set(0, 0);
         while (direction.x == 0 && direction.y == 0)
             direction.Set(Random.Range(-10, 10), Random.Range(-10, 10));
         direction.Normalize();
@@ -17,15 +24,49 @@ public class ChickenBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        countdown -= Time.deltaTime;
-        if (countdown <= 0.0f)
+        if (state == chickenState.Captured)
         {
-            do direction.Set(Random.Range(-10, 10), Random.Range(-10, 10));
-            while (direction.x == 0 && direction.y == 0);
+
+        }
+        else if (state == chickenState.Returning)
+        {
+            direction.Set(henhouse.x - transform.position.x, henhouse.y - transform.position.y);
             direction.Normalize();
-            countdown = 0.7f;
+            //direction.Set(transform.position.x - henhouse.x, transform.position.y - henhouse.y);
+        }
+        else
+        {
+            countdown -= Time.deltaTime;
+            if (countdown <= 0.0f)
+            {
+                do direction.Set(Random.Range(-10, 10), Random.Range(-10, 10));
+                while (direction.x == 0 && direction.y == 0);
+                direction.Normalize();
+                countdown = 0.7f;
+            }
         }
         transform.Translate(direction * Time.deltaTime * speed);
+        if (direction.x < 0.0f)
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+            facingleft = direction.x < 0.0f;
+        }
+        else if (direction.x > 0.0f)
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+            facingleft = direction.x > 0.0f;
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (state == chickenState.inHenhouse && other.gameObject.name == "Henhouse")
+            state = chickenState.Returning;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (state == chickenState.Returning && other.gameObject.name == "Henhouse")
+            state = chickenState.inHenhouse;
     }
 
     void OnCollisionEnter2D(Collision2D other)
