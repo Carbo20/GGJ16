e1c834be3a8ef395254aefbbe13e50dfc0493ca0
carbo20 <carbo20@hotmail.fr>
2016-01-30 03:12:19 +0100

    Sprite + DalleSacrificeMangaer

---
+++ Assets/Scripts/SacrificeDalleManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SacrificeDalleManager : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject[] dalles;
+
+    [SerializeField]
+    private Sprite[] dallesSprites;
+
+    // Use this for initialization
+    void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    public void ChangeDalleSprite(int playerId, int chickenCount)
+    {
+        dalles[playerId].GetComponent<SpriteRenderer>().sprite = dallesSprites[chickenCount];
+    }
+}
