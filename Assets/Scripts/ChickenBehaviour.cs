34c84363d7f162ebb2d2ceb8de7d0d321e6a6f4c
U-otto-pc\otto <bastie_g@epitech.eu>
2016-01-30 21:12:45 +0100

    dynamic chicken speed

--- Assets/Scripts/ChickenBehaviour.cs
+++ Assets/Scripts/ChickenBehaviour.cs
@@ -8,8 +8,10 @@ public class ChickenBehaviour : MonoBehaviour
     public Vector2 direction;
     public Vector2 henhouse;
     public chickenState state;
-    public enum chickenState { inHenhouse, Captured, Returning };
-
+    public enum chickenState { inHenhouse, Captured, Returning, Flying };
+    [SerializeField]
+    private float roamingSpeed = 0.7f;
+    private float runningSpeed = 2.1f;
     [SerializeField]
     public ChickenColors color;
 
@@ -19,28 +21,32 @@ public class ChickenBehaviour : MonoBehaviour
         //state = chickenState.inHenhouse;
         //henhouse.Set(transform.position.x, transform.position.y);
         henhouse.Set(0, 0);
+        speed = roamingSpeed;
         while (direction.x == 0 && direction.y == 0)
             direction.Set(Random.Range(-10, 10), Random.Range(-10, 10));
         direction.Normalize();
     }
 
-    // Update is called once per frame
+    // Update is citalled once per frame
     void Update()
     {
         if (state == chickenState.Captured)
         {
             transform.localPosition = new Vector2(-0.01f, -0.02f);
-
         }
-        else
+        else if (state != chickenState.Flying)
         {
-            if (state == chickenState.Returning)
+            if (transform.position.x > 3)
             {
+                state = chickenState.Returning;
+                speed = runningSpeed;
                 direction.Set(henhouse.x - transform.position.x, henhouse.y - transform.position.y);
                 direction.Normalize();
             }
             else
             {
+                state = chickenState.inHenhouse;
+                speed = roamingSpeed;
                 countdown -= Time.deltaTime;
                 if (countdown <= 0.0f)
                 {
@@ -61,15 +67,6 @@ public class ChickenBehaviour : MonoBehaviour
             }
         }
     }
-    void OnTriggerExit2D(Collider2D other)
-    {
-        Debug.Log("triggerd");
-        if (state != chickenState.Captured && other.gameObject.name == "Henhouse")
-        {
-            Debug.Log("exited");
-            state = chickenState.Returning;
-        }
-    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -81,12 +78,11 @@ public class ChickenBehaviour : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Wall"))
         {
-            Debug.Log("hitsomething: ");
-            direction.x *= -10;
-            direction.y *= -10;
+            //Debug.Log("hitsomething: ");
+            direction.x *= -1;
+            direction.y *= -1;
             direction.Normalize();
         }
-
     }
 
     public ChickenColors GetColor()
